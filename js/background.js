f4829317caab253a9157c4b76f791a8bf20b85b6
Petr Chmelar <petr.chmelar@greycortex.com>
2020-11-07 00:47:41 +0100

    updated readme

--- js/background.js
+++ js/background.js
@@ -48,8 +48,10 @@ const WWW_REGEX = /w+[0-9]*/;
 const IP_REGEX = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
 const IDN_REGEX = /xn--/;
 const BS_REGEX = /[^\.\-_0-9a-z]/g;
+
 // load and prepare the model of up to 44 overlapping bigrams trained from github 
-const MODEL_PATH = "https://raw.githubusercontent.com/greycortex/DomAInt/master/models/bigrams_model_GRU64/model.json"
+const MODEL_PATH = "https://gitlab.greycortex.com/p3/domain/-/raw/rnn/models/domain_bigrams.js/model.json";
+// const MODEL_PATH = "https://raw.githubusercontent.com/greycortex/DomAInt/rnn/models/domain_bigrams.js/model.json";
 // "https://raw.githubusercontent.com/greycortex/DomAIn/master/models/model-M0/model.json";
 
 // global variable for model loading
